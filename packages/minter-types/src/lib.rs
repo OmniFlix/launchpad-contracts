32b325b4ad92292132bc4c0b5dfcba55ba909129
Ninjatosba <adenizcorlu@gmail.com>
2024-01-23 19:32:10 +0300

    add PauseState to minter_types

--- packages/minter-types/src/lib.rs
+++ packages/minter-types/src/lib.rs
@@ -1,5 +1,7 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::{Addr, Coin, Decimal, Timestamp};
+use cosmwasm_std::{Addr, Coin, Decimal, StdError, Storage, Timestamp};
+use cw_storage_plus::Item;
+use thiserror::Error;
 
 #[cw_serde]
 pub struct CollectionDetails {
@@ -16,6 +18,7 @@ pub struct CollectionDetails {
     pub uri_hash: String,
     pub data: String,
     pub transferable: bool,
+    // FE: Collection:"Badkids" each token name "BadKid" #token_id
     pub token_name: String,
 }
 
@@ -71,4 +74,82 @@ pub struct Config {
     pub admin: Addr,
     pub whitelist_address: Option<Addr>,
     pub token_limit: Option<u32>,
+}
+
+#[derive(Error, Debug, PartialEq)]
+pub enum PauseError {
+    #[error(transparent)]
+    Std(#[from] StdError),
+
+    #[error("contract is paused")]
+    Paused {},
+
+    #[error("unauthorized pauser ({sender})")]
+    Unauthorized { sender: Addr },
+}
+
+pub struct PauseState<'a> {
+    pub paused: Item<'a, bool>,
+    pub pausers: Item<'a, Vec<Addr>>,
+}
+
+impl<'a> PauseState<'a> {
+    /// Creates a new pause orchestrator using the provided storage
+    /// keys.
+    pub const fn new(paused_key: &'a str, pausers_key: &'a str) -> Self {
+        Self {
+            paused: Item::new(paused_key),
+            pausers: Item::new(pausers_key),
+        }
+    }
+
+    /// Sets a new pauser who may pause the contract. If the contract
+    /// is paused, it is unpaused.
+    pub fn set_pausers(
+        &self,
+        storage: &mut dyn Storage,
+        sender: Addr,
+        pausers: Vec<Addr>,
+    ) -> Result<(), PauseError> {
+        self.error_if_unauthorized(storage, &sender)?;
+        self.pausers.save(storage, &pausers)?;
+        Ok(())
+    }
+
+    /// Errors if the module is paused, does nothing otherwise.
+    pub fn error_if_paused(&self, storage: &dyn Storage) -> Result<(), PauseError> {
+        if self.paused.load(storage)? {
+            Err(PauseError::Paused {})
+        } else {
+            Ok(())
+        }
+    }
+    pub fn error_if_unauthorized(
+        &self,
+        storage: &dyn Storage,
+        sender: &Addr,
+    ) -> Result<(), PauseError> {
+        let pausers = self.pausers.load(storage)?;
+        if !pausers.contains(sender) {
+            Err(PauseError::Unauthorized {
+                sender: sender.clone(),
+            })
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn pause(&self, storage: &mut dyn Storage, sender: &Addr) -> Result<(), PauseError> {
+        self.error_if_paused(storage)?;
+        self.error_if_unauthorized(storage, sender)?;
+        self.paused.save(storage, &true)?;
+        Ok(())
+    }
+
+    pub fn unpause(&self, storage: &mut dyn Storage, sender: &Addr) -> Result<(), PauseError> {
+        self.error_if_paused(storage)?;
+        self.error_if_unauthorized(storage, sender)?;
+        self.paused.save(storage, &false)?;
+        Ok(())
+    }
 }
